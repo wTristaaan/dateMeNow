e0b433daaba9d951866d5ac3c67a551738cad5df
Tristan <tristan.joubert@mail.com>
2025-01-27 17:53:39 +0100

    [ADD] configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'dateMeNow',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      backgroundColor: "#0B053000",
+      launchFadeOutDuration: 0,
+      splashFullScreen: true,
+      useDialog: false,
+    },
+  },
 };
 
 export default config;
